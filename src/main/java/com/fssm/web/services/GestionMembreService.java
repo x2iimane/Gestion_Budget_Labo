7ed3bce8b6a8c5b3c62d96ed895441f274932177
x2iimane <rafiqimane.mail@gmail.com>
2022-06-20 22:49:43 +0100

    ok

--- src/main/java/com/fssm/web/services/GestionMembreService.java
+++ src/main/java/com/fssm/web/services/GestionMembreService.java
@@ -2,14 +2,30 @@ package com.fssm.web.services;
 
 import com.fssm.web.entities.Laboratoire;
 import com.fssm.web.entities.Membre;
+import com.fssm.web.enums.Grade;
+import com.fssm.web.enums.Specialite;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface GestionMembreService {
 
-    public List<Membre> getAllMembresLabo (String titreLabo) ;
+    public List<Membre> getMembresByLabo(String titreLabo);
 
+    public List<Object[]> getAllmemebresLabo();
 
+    public Membre AddNewMembre(String nom,
+                               String prenom,
+                               Grade grade,
+                               Specialite specialite,
+                               LocalDate dateNaissance
+    );
+
+    public Membre UpdateMembre(Long id);
+
+    public void DeleteMembre(Long id);
+
+    public Membre AffectMembreLabo(int idMembre , String idLabo);
 
 
 
--- src/main/java/com/fssm/web/services/GestionMembreServiceImp.java
+++ src/main/java/com/fssm/web/services/GestionMembreServiceImp.java
@@ -3,11 +3,14 @@
  */
 package com.fssm.web.services;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
 import com.fssm.web.entities.Membre;
+import com.fssm.web.enums.Grade;
+import com.fssm.web.enums.Specialite;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,10 +23,33 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class GestionMembreServiceImp implements GestionMembreService {
 
-
     @Override
-    public List<Membre> getAllMembresLabo(String titreLabo) {
+    public List<Membre> getMembresByLabo(String titreLabo) {
         return null;
     }
 
+    @Override
+    public List<Object[]> getAllmemebresLabo() {
+        return null;
+    }
+
+    @Override
+    public Membre AddNewMembre(String nom, String prenom, Grade grade, Specialite specialite, LocalDate dateNaissance) {
+        return null;
+    }
+
+    @Override
+    public Membre UpdateMembre(Long id) {
+        return null;
+    }
+
+    @Override
+    public void DeleteMembre(Long id) {
+
+    }
+
+    @Override
+    public Membre AffectMembreLabo(int idMembre, String idLabo) {
+        return null;
+    }
 }
